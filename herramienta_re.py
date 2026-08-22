2eaa3e03f78a6d6869e849ae8320385ca5e649fe
Criskroll <dev.criskroll@gmail.com>
2026-08-22 14:28:19 -0400

    se incorporo else para cuando no se encuentra la palabra

--- herramienta_re.py
+++ herramienta_re.py
@@ -6,4 +6,12 @@ import re
 texto = "hola a ti que lees el codigo, sigueme para aprender mas"
 resultado = re.search("sigueme", texto)
 
-print(resultado)
+# Las búsquedas con re.search() son sensibles a mayúsculas y minúsculas (case-sensitive). 
+# Al buscar "SIGUEME" en un texto que contiene "sigueme", 
+# la función no encuentra ninguna coincidencia y devuelve None. 
+# Por lo tanto, el programa se salta el if y ejecuta el else, imprimiendo "Palabra no encontrada".
+
+if resultado:
+    print(resultado)
+else:
+    print("Palabra no encontrada")
